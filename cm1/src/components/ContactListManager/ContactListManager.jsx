3827e10cd03f15efb80993365dc56d86d7ac7ba1
leevitiihonen <leevislettu@gmail.com>
2025-02-09 16:05:52 +0200

    package-lock.json added back

--- cm1/src/components/ContactListManager/ContactListManager.jsx
+++ cm1/src/components/ContactListManager/ContactListManager.jsx
@@ -71,33 +71,3 @@ function ContactListManager() {
 
 export default ContactListManager;
 
-/*
-self assessment:
-
-Problem 1, Wrong casing :
-
-return (
-    <div className="Contact-collection">
-      <h1>Contact List Manager</h1>
-
-Here "Contact-collection" was different than the one in css file ("contact-collection") so the css didn't show up on the page.
-
-(fixed version) 
-
-return (
-    <div className="contact-collection">
-      <h1>Contact List Manager</h1>
-
-
-Lesson learned: Be more aware of upper/lowercase letters in coding while troubleshooting. 
-
-Successess:
-
-After lessons and by following example codes given in the coding marathon this exercise went well.
-I got to a desired outcome without many problems.
-
-Areas for Improvement:
-
-Better understanding of the code, adding more variability and functionality + creating a even better UI with CSS.
-
-*/
